f429aab66f22fa6b2b740609a5ee70ee23f00a29
lokesh-katari <lokeshkatari921@outlook.com>
2024-01-28 17:10:56 +0530

    hii

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+const mongoose = require('mongoose');       
+cloudinary.config({ 
+  cloud_name: 'dp0i92tjf', 
+  api_key: '227668771945544', 
+  api_secret: 'N9IKKoyrz7bsj2A-LPCu24R6XCg' 
+});
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+mongoose.connect('mongodb+srv://lokeshkatari921:GaIpVOppyLAOe6TU@cluster0.3edicq2.mongodb.net/?retryWrites=true&w=majority').then(()=>{ 
+    console.log(" DB connection successfull ");
+} );
+
+
+
+// Define a simple video schema
+const videoSchema = new mongoose.Schema({
+    name: String,
+    url: String
+  });
+  const Video = mongoose.model('Video', videoSchema);
+
+// EJS setup
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+    }
+);
+
+app.post('/upload', upload.single('video'), (req, res) => {
+    // Upload video to Cloudinary
+    cloudinary.uploader.upload_stream({ resource_type: 'video' }, (error, result) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send('Error uploading video');
+      } else {
+        // Save video details to MongoDB
+        const newVideo = new Video({
+          name: req.file.originalname, // Assuming you want to store the original file name
+          url: result.secure_url
+        });
+  
+        newVideo.save().then(() => res.send('Video saved to MongoDB'));
+      }
+    }).end(req.file.buffer);
+  });
+
+
+  app.get('/videos', async (req, res) => {
+    try {
+      const videos = await Video.find();
+      res.render('videos', { videos });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error fetching videos from the database');
+    }
+  });
+
+  
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+    }
+);
